d9172092953c6f8ab91ba3e330065058eeefe14c
xluffy <quang@2meo.com>
2025-05-07 23:27:39 +0700

    Improve flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "home manager configuration";
+  description = "Home manager configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
@@ -9,28 +9,27 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
-    homeConfigurations = {
-      "quanggg@xluffys-MacBook-Air.local" = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs {
-          system = "aarch64-darwin";
-        };
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      generateHomeConfig = username: home-manager.lib.homeManagerConfiguration {
+        pkgs = pkgs;
         modules = [ ./home-manager/home.nix ];
       };
-
-      "quanggg@xluffys-mini.local" = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs { system = "aarch64-darwin"; };
-
-        modules = [ ./home-manager/home.nix ];
+    in
+    {
+      homeConfigurations = {
+        "quanggg@xluffys-MacBook-Air.local" = generateHomeConfig "quanggg";
+        "quanggg@xluffys-mini.local" = generateHomeConfig "quanggg";
       };
-    };
-
-    devShells = {
-      aarch64-darwin.default = import ./shell.nix {
-        pkgs = import nixpkgs {
-          system = "aarch64-darwin";
+      devShells = {
+        aarch64-darwin.default = import ./shell.nix {
+          pkgs = pkgs;
         };
       };
     };
-  };
 }

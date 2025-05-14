a333206c21153fbf9d2e490d9b4f6af9fa52e99f
xluffy <quang@2meo.com>
2025-05-14 15:20:32 +0700

    Add new font.nix

---
+++ home-manager/modules/core/font.nix
@@ -0,0 +1,9 @@
+{ pkgs-unstable, ... }:
+{
+
+  home.packages = with pkgs; [
+    pkgs-unstable.nerd-fonts.jetbrains-mono
+    pkgs-unstable.hackgen-nf-font
+    pkgs-unstable.nerd-font-patcher
+  ];
+}

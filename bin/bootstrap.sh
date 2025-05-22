218a6e5b8d59b38e554a6c987c66162f0956c697
xluffy <quang@2meo.com>
2025-05-22 11:05:38 +0700

    Bootstrap system settings for macos

---
+++ bin/bootstrap.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env
+
+sh <(curl -L https://nixos.org/nix/install) --daemon --yes
+
+defaults write com.apple.screencapture type jpg
+# do not open previous previewed files (e.g. PDFs) when opening a new one
+defaults write com.apple.Preview ApplePersistenceIgnoreState YES
+
+# show Library folder
+chflags nohidden ~/Library
+
+# show hidden files
+defaults write com.apple.finder AppleShowAllFiles YES
+
+# show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+killall Finder;
--- bin/macos.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env
-
-sh <(curl -L https://nixos.org/nix/install) --daemon --yes

de11776a9a1387fe0ddf754535b7fcf3ea7c3436
xluffy <quang@2meo.com>
2025-05-27 13:33:10 +0700

    Add _1password-gui

--- home-manager/modules/core/gui.nix
+++ home-manager/modules/core/gui.nix
@@ -4,6 +4,7 @@
   ...
 }: {
   home.packages = with pkgs; [
+    pkgs-unstable._1password-gui
     pkgs-unstable.betterdisplay
     pkgs-unstable.karabiner-elements
     pkgs-unstable.telegram-desktop

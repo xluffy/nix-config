70015d81781e1c05e7a57d0812390a716942a067
xluffy <quang@2meo.com>
2025-06-23 22:20:27 +0700

    Add flameshot for screenshot

--- home-manager/modules/core/gui.nix
+++ home-manager/modules/core/gui.nix
@@ -7,6 +7,7 @@
     _1password-gui
     pkgs-unstable.betterdisplay
     pkgs-unstable.karabiner-elements
+    pkgs-unstable.flameshot
     pkgs-unstable.telegram-desktop
     pkgs-unstable.tailscale
   ];

077a1a9d18cbfae09bbbfa9987b2e1b18aa76234
xluffy <quang@2meo.com>
2026-05-24 13:15:10 +0700

    refactor: simplify and modularize home-manager configuration

---
+++ home-manager/modules/core/default.nix
@@ -0,0 +1,14 @@
+{...}: {
+  imports = [
+    ./cli.nix
+    ./dev.nix
+    ./dev-db.nix
+    ./dev-llm.nix
+    ./dev-ops.nix
+    ./dev-php.nix
+    ./dev-terraform.nix
+    ./font.nix
+    ./gui.nix
+    ./nix.nix
+  ];
+}

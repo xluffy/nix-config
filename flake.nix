3701a57cdbfa993b367797a9c29c08ba5dd36c06
xluffy <quang@2meo.com>
2026-09-24 11:34:01 +0700

    feat(omp): bump omp 18.2.11 -> 18.3.0

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     llm-agents.url = "github:numtide/llm-agents.nix";
 
-    omp.url = "github:can1357/oh-my-pi/v18.2.11";
+    omp.url = "github:can1357/oh-my-pi/v18.3.0";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
